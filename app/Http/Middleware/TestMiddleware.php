bcb4a80b93df62614d4b38604ee3551639e68fe9
Josh Reed <jreed@thedmsgrp.com>
2018-10-25 15:41:09 -0600

    example

---
+++ app/Http/Middleware/TestMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class TestMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string|null  $guard
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = null)
+    {
+        session()->put('foo', request()->get('foo'));
+        $response = $next($request);
+        return $response;
+    }
+}
